fb5b0649aae36df0077a9b2e98ac69edbfaf57c6
Sukesh Dash <sukesjh@yahoo.com>
2024-10-17 20:31:58 +0530

    DiamondKata solution

---
+++ DiamondKataSolution/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+class Diamond
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a character (A-Z):");
+        char inputChar = Char.ToUpper(Console.ReadKey().KeyChar);
+        Console.WriteLine();
+
+        if (IsValidInput(inputChar))
+        {
+            PrintDiamondPattern(inputChar);
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("Invalid input! Please enter a letter from A to Z.");
+        }
+    }
+
+    static bool IsValidInput(char input)
+    {
+        return input >= 'A' && input <= 'Z';
+    }
+
+    static void PrintDiamondPattern(char midpoint)
+    {
+        int midpointPosition = midpoint - 'A';
+        int counter = 0;
+        do
+        {
+            PrintDiamondRow(counter, midpointPosition);
+            counter++;
+        } while (counter <= midpointPosition);
+
+
+        for (int counterDown = midpointPosition - 1; counterDown >= 0; counterDown--)
+        {
+            PrintDiamondRow(counterDown , midpointPosition);
+        }
+    }
+
+    static void PrintDiamondRow(int currentRow, int midpointPosition)
+    {
+        int totalWidth = midpointPosition * 2 + 1;
+        char currentChar = (char)('A' + currentRow);
+
+        for (int counter = 0; counter < totalWidth; counter++)
+        {
+            if (counter == midpointPosition - currentRow || counter == midpointPosition + currentRow)
+            {
+                Console.Write(currentChar);
+            }
+            else
+            {
+                Console.Write(' ');
+            }
+        }
+
+        Console.WriteLine();
+    }
+}
